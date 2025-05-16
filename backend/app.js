2912707e9ffcef0155e30c73dc64b1c393c18e56
t_hrithik <hrithik.ranjan@lavainternational.in>
2025-05-16 18:53:21 +0530

    Product routes and controllers created

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
 
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+app.use("/api/products",productRoutes);
 
 module.exports = app;
---
+++ backend/controllers/productController.js
@@ -0,0 +1,62 @@
+const { Product } = require("../models");
+
+// Create new Product
+exports.createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Get all products
+exports.getAllProducts = async(req,res)=>{
+    try {
+        const products = await Product.findAll();
+        res.json(products)
+    } catch (err) {
+        res.status(500).json({error:err.message})
+    }
+}
+// Get product by ID
+exports.getProductById = async(req,res)=>{
+    try {
+        const product = await Product.findByPk(req.params.id);
+        if (!product) {
+            return res.status(404).json({error:"Product not found"});
+        }
+        res.json(product);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+// Update product
+exports.updateProduct = async (req,res) => {
+    try {
+        const [updated] = await Product.update(req.body,{
+            where: {id:req.params.id}
+        });
+        if (!updated) {
+            return res.status(404).json({error:"Product not found"});
+        }
+        const updatedProduct = await Product.findByPk(req.params.id);
+        res.json(updatedProduct);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
+//Delete Product
+exports.deleteProduct = async(req,res) =>{
+    try {
+    const deleted = await Product.destroy({
+        where:{id:req.params.id}
+    });
+    if (!deleted) return res.status(404).json({ error: "Product not found" });
+    res.json({ message: "Product deleted" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
