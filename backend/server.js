e044ba29ca2710a1f1c7d0ac724a63b2652ca202
t_hrithik <hrithik.ranjan@lavainternational.in>
2025-05-20 16:23:10 +0530

    minor fix for migration

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,21 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 
+const runMigrations = async () => {
+  const { exec } = require('child_process');
+  exec('npx sequelize-cli db:migrate', (error, stdout, stderr) => {
+    if (error) {
+      console.error('Migration failed:', stderr);
+    } else {
+      console.log('Migration output:\n', stdout);
+    }
+  });
+};
+
+
+runMigrations();
+
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
