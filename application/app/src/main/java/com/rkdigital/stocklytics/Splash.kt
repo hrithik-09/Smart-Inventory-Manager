0281d0ca2762715b88d0eb257655f9427f3f45f6
t_hrithik <hrithik.ranjan@lavainternational.in>
2025-05-20 11:15:32 +0530

    Splash Screen Created

---
+++ application/app/src/main/java/com/rkdigital/stocklytics/Splash.kt
@@ -0,0 +1,23 @@
+package com.rkdigital.stocklytics
+
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val logo = findViewById<ImageView>(R.id.logoImage)
+        val glow = findViewById<ImageView>(R.id.glowEffect)
+        val glowAnimation = AnimationUtils.loadAnimation(this@Splash, R.anim.glow_pulse).also {
+            glow.startAnimation(it)
+            glow.animate().alpha(0.3f).duration = 1000
+        }
+        logo.startAnimation(glowAnimation)
+    }
+}
