2a55b5ca87988d1afa5a9003cf367e7fac79260e
t_hrithik <hrithik.ranjan@lavainternational.in>
2025-05-21 14:30:16 +0530

    missing middleware path added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authenticateToken = require('../middleware/authMiddleware')
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
